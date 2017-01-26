f90a24dae31cde8816e2e94be8baa7c5620aecc6
dkeske <danielkeske@hotmail.com>
2017-01-26 11:55:21 +0100

    Store_matches

--- DataImport/store_matches_pg.py
+++ DataImport/store_matches_pg.py
@@ -20,9 +20,63 @@ ON CONFLICT DO NOTHING;
 """
 
 SQL_GET = """
-SELECT * FROM matches WHERE meta_id = %s;
+SELECT * FROM matches WHERE ref_id = %s;
 """
 
 SQL_DELETE = """
-DELETE FROM matches WHERE meta_id = %s;
+DELETE FROM matches WHERE ref_id = %s;
 """
+
+
+class store:
+    def __init__(self, **kwargs):
+        self.con = psycopg2.connect(**kwargs)
+        self.cur = self.con.cursor()
+        self.q = []  # fresh list
+
+    def table_create(self):
+        self.cur.execute(SQL_CREATE);
+        self.con.commit();
+        return self.cur.statusmessage
+
+    def table_drop(self):
+        self.cur.execute(SQL_DROP);
+        self.con.commit()
+        return self.cur.statusmessage
+
+    def queue_match(self, ref_id, meta_id):
+        "Queue data for insertion"
+        self.q.append([meta_id, ref])
+
+    def flush(self):
+        "Write out queue to DB"
+        n = len(self.q)
+        self.cur.executemany(SQL_INSERT, self.q);
+        self.con.commit()
+        self.q = []  # clear queue
+        return self.cur.statusmessage
+
+    def get(self, ref_id):
+        "Lookup citations for a paper"
+        self.cur.execute(SQL_GET, (ref_id,))
+        return self.cur.fetchall()
+
+    def delete(self, ref_id):
+        "Delete all references of a paper in the db"
+        self.cur.execute(SQL_DELETE, (ref_id,))
+        self.con.commit()
+        return self.cur.statusmessage
+
+    def close(self):
+        self.con.close()
+
+
+if __name__ == "__main__":
+    s = store(user="rw", database="rw")
+    print(s.table_create())
+    print(s.queue_match("test-ref-id", "test-meta"))
+    print(s.queue_match("test-ref-id", "test-meta-2"))
+    print(s.flush())
+    print(s.get("test-ref-id"))
+    print(s.delete("test-ref-id"))
+    print(s.close())
