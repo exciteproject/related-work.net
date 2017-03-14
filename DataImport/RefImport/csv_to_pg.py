15985f31e4e3a97d48e8562c9dae1ffae6f0f963
dkeske <danielkeske@hotmail.com>
2017-03-14 11:57:42 +0100

    Added a parameter for destination table

--- DataImport/RefImport/csv_to_pg.py
+++ DataImport/RefImport/csv_to_pg.py
@@ -2,12 +2,14 @@ from store_refs_pg_m import store
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('csv_file', help='path to csv file with references', type=str, default='references.csv')
+parser.add_argument('csv_file', help='path to csv file with references', type=str)
+parser.add_argument('dest_table', help='destination table to import to', type=str)
 args = parser.parse_args()
 csv_file = args.csv_file
+dest_table = args.dest_table
 s = store(user="rw", database="rw")
 f = open(csv_file, 'r')
-s.cur.copy_from(f, 'refs_m', sep=',')
+s.cur.copy_from(f, dest_table, sep=',')
 s.con.commit()
 f.close()
 
