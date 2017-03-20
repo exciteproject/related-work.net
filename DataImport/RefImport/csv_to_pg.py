6eb94975edeb7bb0496e7fb99a119ef565ef4149
dkeske <danielkeske@hotmail.com>
2017-03-20 12:10:40 +0100

    csv_to_pg import script takes table as an argument

--- DataImport/RefImport/csv_to_pg.py
+++ DataImport/RefImport/csv_to_pg.py
@@ -1,5 +1,6 @@
-from store_refs_pg_m import store
+import psycopg2
 import argparse
+import traceback
 
 parser = argparse.ArgumentParser()
 parser.add_argument('csv_file', help='path to csv file with references', type=str)
@@ -7,9 +8,13 @@ parser.add_argument('dest_table', help='destination table to import to', type=st
 args = parser.parse_args()
 csv_file = args.csv_file
 dest_table = args.dest_table
-s = store(user="rw", database="rw")
+con = psycopg2.connect(user="rw", database="rw")
+cur = con.cursor()
 f = open(csv_file, 'r')
-s.cur.copy_from(f, dest_table, sep=',')
-s.con.commit()
+try:
+    cur.copy_from(f, dest_table, sep=',')
+    con.commit()
+except psycopg2.ProgrammingError:
+    traceback.print_exc()
 f.close()
 
