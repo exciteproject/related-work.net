3061927d4e4eec1aeddef0eb30d1478251f5257a
dkeske <danielkeske@hotmail.com>
2017-03-21 12:00:54 +0100

    Counter field added to perserve original ordering of references

--- DataImport/RefImport/import.sh
+++ DataImport/RefImport/import.sh
@@ -9,6 +9,7 @@ IMPORT_TBL_NAME=refs_m
 
 # create table if needed
 psql -U rw -c "CREATE TABLE IF NOT EXISTS $IMPORT_TBL_NAME (
+counter INTEGER,
 ref_id VARCHAR(50),
 ref_text TEXT)"
 
@@ -16,7 +17,7 @@ ref_text TEXT)"
 psql -U rw -c "CREATE INDEX ref_id_idx ON $IMPORT_TBL_NAME(ref_id)"
 # create views
 psql -U rw -c "CREATE OR REPLACE VIEW view_$IMPORT_TBL_NAME as
-SELECT * FROM $IMPORT_TBL_NAME ORDER BY ref_text ASC"
+SELECT * FROM $IMPORT_TBL_NAME ORDER BY counter ASC"
 
 # import data into table
 python csv_to_pg.py $IMPORT_DATA $IMPORT_TBL_NAME
--- DataImport/RefImport/txt_to_csv.py
+++ DataImport/RefImport/txt_to_csv.py
@@ -3,7 +3,9 @@ import csv
 files = os.listdir()[:-1]
 ssoar = open('ssoar.csv', 'w', newline='', encoding='utf-8')
 outputWriter = csv.writer(ssoar, delimiter='\t')
+i = 0
 for file in files:
     f = open(file, encoding='utf-8')
     for line in f:
-        outputWriter.writerow([file.split('.')[0], line[:-1]])
+        outputWriter.writerow([i, file.split('.')[0], line[:-1]])
+        i += 1
