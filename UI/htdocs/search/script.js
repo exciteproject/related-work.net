a840f53c3b9099afc677c739d1b5b82b04d14234
dkeske <danielkeske@hotmail.com>
2017-05-23 13:13:09 +0200

    Added loader to search

--- UI/htdocs/search/script.js
+++ UI/htdocs/search/script.js
@@ -1,19 +1,20 @@
-window.onhashchange = function() {
+window.onhashchange = function () {
     loadResults(window.location.hash.substring(1) || "colorful");
 };
-function loadResults(query){
+function loadResults(query) {
     $("#ul_results").empty();
     $("#title_q").text(query);
     window.history.pushState('Page', 'Title', '/search/#' + query);
     $.get("/search_arxiv/" + query, function (data, status) {
-        if(data.length == 0){
+        $("#loader").hide();
+        if (data.length == 0) {
             $("#ul_results").append(
                 $('<li>').text("No results found"));
         }
         data.forEach(function (item, index) {
             $("#ul_results").append(
                 $('<li>').append(
-                    $("<a>").attr('href', '/preview/#'+item.meta_id).text(item.author + ', ').append(
+                    $("<a>").attr('href', '/preview/#' + item.meta_id).text(item.author + ', ').append(
                         $("<em>").text(item.title)
                     ).append("<span> (Cited: " + item.citations + ")</span>")
                 )
@@ -25,7 +26,8 @@ function loadResults(query){
 $(document).ready(function () {
     loadResults(window.location.hash.substring(1) || "colorful");
 
-    $("#form-search").submit(function( event ) {
+    $("#form-search").submit(function (event) {
+        $("#loader").show();
         var query = $("#q").val();
         loadResults(query);
         event.preventDefault();
