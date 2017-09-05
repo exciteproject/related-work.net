ee00275e1bfc9aa975a7b6859ee84e8e70d51b9e
dkeske <danielkeske@hotmail.com>
2017-09-05 13:59:18 +0200

    Moved pdf download to be last, refreshing citations count

--- DataImport/daily_arxiv_update.sh
+++ DataImport/daily_arxiv_update.sh
@@ -12,10 +12,12 @@ mv "$DATE:$TOMORROW" "$DATE"
 cp "$DATE" "../meta_daily_unpacked/$YEAR/$DATE.json"
 cd ~/related-work.net/DataImport/
 python daily_meta.py insert_meta
-python daily_meta.py download_pdf
 python daily_meta.py download_source
 # cd /EXCITE/datasets/arxiv/source_daily/$DATE
 # for f in *; do mv "$f" "$f.tar.gz"; done
 python daily_meta.py extract_refs
 #
 python daily_meta.py match_refs
+python daily_meta.py download_pdf
+
+psql -U rw -c "REFRESH MATERIALIZED VIEW view_citations_count"
